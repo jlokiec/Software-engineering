67de3546981bb1e7b4376e1cadf674ac58a7170e
Jakub Łokieć <jakublokiec@wp.pl>
2018-10-25 16:54:23 +0200

    Create abstract DAO implementation

---
+++ src/main/java/dao/AbstractDaoImpl.java
@@ -0,0 +1,49 @@
+package dao;
+
+import model.AbstractModel;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class AbstractDaoImpl<T extends AbstractModel> implements InterfaceDao<T> {
+    private EntityManager entityManager;
+    private Class<T> genericClass;
+
+    public AbstractDaoImpl(Class<T> genericClass) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Pizzeria-Server");
+        entityManager = entityManagerFactory.createEntityManager();
+        this.genericClass = genericClass;
+    }
+
+    @Override
+    public T create(T modelObject) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(modelObject);
+        entityManager.getTransaction().commit();
+
+        return modelObject;
+    }
+
+    @Override
+    public T read(int id) {
+        return entityManager.find(genericClass, id);
+    }
+
+    @Override
+    public T update(T modelObject) {
+        entityManager.getTransaction().begin();
+        T updatedObject = entityManager.merge(modelObject);
+        entityManager.getTransaction().commit();
+
+        return updatedObject;
+    }
+
+    @Override
+    public void delete(int id) {
+        entityManager.getTransaction().begin();
+        T foundObject = entityManager.find(genericClass, id);
+        entityManager.remove(foundObject);
+        entityManager.getTransaction().commit();
+    }
+}
