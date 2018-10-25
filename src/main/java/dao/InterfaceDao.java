37a47e5cc11a547b411b11a9142453781bcb9c98
Jakub Łokieć <jakublokiec@wp.pl>
2018-10-25 16:54:01 +0200

    Create interface for DAO

---
+++ src/main/java/dao/InterfaceDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import model.AbstractModel;
+
+public interface InterfaceDao<T extends AbstractModel> {
+    T create(T modelObject);
+
+    T read(int id);
+
+    T update(T modelObject);
+
+    void delete(int id);
+}
