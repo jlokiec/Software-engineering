aef46dad6021e13b40fcec30dcb1b6b48677d3aa
Jakub Lokiec <jakublokiec@wp.pl>
2018-10-26 19:34:35 +0200

    Apply minor changes in QuestionDao
    * Add square brackets to table name in SQL query
    * Declare Query and return List of its result

--- src/main/java/dao/QuestionDao.java
+++ src/main/java/dao/QuestionDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import model.Question;
 
+import javax.persistence.Query;
 import java.util.List;
 
 public class QuestionDao extends AbstractDaoImpl<Question> {
@@ -10,7 +11,8 @@ public class QuestionDao extends AbstractDaoImpl<Question> {
     }
 
     public List<Question> getAll() {
-        String sqlQuery = "select * from pizzeria.dbo.question";
-        return (List<Question>) entityManager.createNativeQuery(sqlQuery, Question.class).getResultList();
+        String sqlQuery = "select * from [pizzeria].[dbo].[question]";
+        Query query = entityManager.createNativeQuery(sqlQuery, Question.class);
+        return (List<Question>) query.getResultList();
     }
 }
