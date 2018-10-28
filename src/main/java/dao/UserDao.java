3778afbd0fd7d96d47893f15dc3c169dfcd0968b
Jakub Łokieć <jakublokiec@wp.pl>
2018-10-28 16:12:42 +0100

    Add check when trying to update user details
    User needs to be active and logged in to change their details.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -68,25 +68,29 @@ public class UserDao extends AbstractDaoImpl<User> {
             return null;
         }
 
-        String newName = userDetails.getName();
-        String newSurname = userDetails.getSurname();
-        String newPhone = userDetails.getPhone();
-        String newEmail = userDetails.getEmail();
+        if (user.isActive() && user.isLoggedIn()) {
+            String newName = userDetails.getName();
+            String newSurname = userDetails.getSurname();
+            String newPhone = userDetails.getPhone();
+            String newEmail = userDetails.getEmail();
 
-        if (newName != null && !newName.equals("")) {
-            user.setName(newName);
-        }
-        if (newSurname != null && !newSurname.equals("")) {
-            user.setSurname(newSurname);
-        }
-        if (newPhone != null && !newPhone.equals("")) {
-            user.setPhone(newPhone);
-        }
-        if (newEmail != null && !newEmail.equals("")) {
-            user.setEmail(newEmail);
+            if (newName != null && !newName.equals("")) {
+                user.setName(newName);
+            }
+            if (newSurname != null && !newSurname.equals("")) {
+                user.setSurname(newSurname);
+            }
+            if (newPhone != null && !newPhone.equals("")) {
+                user.setPhone(newPhone);
+            }
+            if (newEmail != null && !newEmail.equals("")) {
+                user.setEmail(newEmail);
+            }
+
+            return update(user);
         }
 
-        return update(user);
+        return null;
     }
 
     public boolean login(final UserNickAndPassword userNickAndPassword) {
