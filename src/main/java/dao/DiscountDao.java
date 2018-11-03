dbb5013ddc482edf97b2bda44c21d4bc93f75a7d
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-03 19:02:56 +0100

    Add getAllDiscounts method to DiscountDao

--- src/main/java/dao/DiscountDao.java
+++ src/main/java/dao/DiscountDao.java
@@ -3,6 +3,8 @@ package dao;
 import model.Discount;
 
 import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import java.util.List;
 
 public class DiscountDao extends AbstractDaoImpl<Discount> {
     public DiscountDao() {
@@ -33,5 +35,11 @@ public class DiscountDao extends AbstractDaoImpl<Discount> {
         update(discount);
 
         return true;
+    }
+
+    public List<Discount> getAll(){
+        String sqlQuery = "select * from [pizzeria].[dbo].[discount]";
+        Query query = entityManager.createNativeQuery(sqlQuery, Discount.class);
+        return query.getResultList();
     }
 }
